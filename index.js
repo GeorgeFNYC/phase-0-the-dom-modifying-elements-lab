f2ffbc78dac5065160670935ba4f50d586245a78
George Fernandez <fernandezg704@gmail.com>
2022-09-08 15:26:44 -0400

    commit tree 246c70bf1c13650662176580bd450640e5fc280f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+let main = document.querySelector('#main');
+
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory')
+newHeader.textContent = 'GEORGE is the champion';
+
+
+document.append(newHeader)
